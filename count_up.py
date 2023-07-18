ee57076d70eab8353b6d325015d43a8f36c5de06
Porsche Parcher <130427863+porscheparcher@users.noreply.github.com>
2023-07-18 09:37:08 -0600

    Add files via upload

---
+++ count_up.py
@@ -0,0 +1,22 @@
+def count_up(start, stop):
+    """Print all numbers from start up to and including stop.
+
+    For example:
+
+        count_up(5, 7)
+
+   should print:
+
+        5
+        6
+        7
+    """
+
+    # YOUR CODE HERE
+    x = 5
+    while x <=7:
+        print(x)
+        x = x + 1
+
+
+count_up(5, 7)        
